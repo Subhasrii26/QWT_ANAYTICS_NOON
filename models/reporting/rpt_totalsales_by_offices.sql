cccdbd0385ce0f69fc5a959c2063dcef9b7f257d
Subhasrii P <Subhasrii.Padmanaban@cognizant.com>
2025-02-13 07:11:51 +0000

    dbt session

--- models/reporting/rpt_totalsales_by_offices.sql
+++ models/reporting/rpt_totalsales_by_offices.sql
@@ -10,7 +10,7 @@ sum(o.linesalesamount) as total_sales,
 avg(o.margin) as avg_margin
 from {{ref("fct_orders")}} as o
 join {{ref("dim_customers")}} as c on o.customerid = c.customerid
-join {{ref("dim_employee")}} as e on o.employeeid = e.empid
+join {{ref("dim_employee")}} as e on o.employeeid = e.employee_id
 where e.country = '{{var('v_country','Sweden')}}'
 group by e.country, c.companyname,c.contactname
 order by total_sales desc
